044b9606c50d333462e873a7be970f895bcd43cc
IgnacioEscobar <ignacionicolas.e.c@gmail.com>
2018-07-23 16:43:09 -0300

    Validado de columnas

--- src/main/java/sqlgen/ConversorExcel.java
+++ src/main/java/sqlgen/ConversorExcel.java
@@ -13,8 +13,10 @@ public class ConversorExcel {
             Workbook workbook = WorkbookFactory.create(new File(fileURL));
             Sheet sheet1 = workbook.getSheetAt(0);
             for (Row row : sheet1) {
-                Cell firstCell = row.getCell(0);
-                System.out.println(getStringContent(firstCell));
+                if(filaValida(row)) {
+                    Cell firstCell = row.getCell(0);
+                    System.out.println(getStringContent(firstCell));
+                }
 
             }
         } catch (IOException e) {
@@ -25,23 +27,34 @@ public class ConversorExcel {
     }
 
     private static String getStringContent(Cell cell){
-        if (cell != null) {
-            switch (cell.getCellTypeEnum()) {
-                case STRING:
-                    return cell.getRichStringCellValue().getString();
-                case NUMERIC:
-                    return Double.toString(cell.getNumericCellValue());
-                case BOOLEAN:
-                    return cell.getBooleanCellValue()? "1":"0";
-                case FORMULA:
-                    return cell.getCellFormula();
-                case BLANK:
-                    return "";
-                default:
-                    return "";
-            }
+        switch (cell.getCellTypeEnum()) {
+            case STRING:
+                return cell.getRichStringCellValue().getString();
+            case NUMERIC:
+                return Double.toString(cell.getNumericCellValue());
+            case BOOLEAN:
+                return cell.getBooleanCellValue()? "1":"0";
+            case FORMULA:
+                return cell.getCellFormula();
+            case BLANK:
+                return "";
+            default:
+                return "";
         }
-        return " ";
+    }
+
+    private static Boolean filaValida(Row fila){
+        Cell celdaCodigo = fila.getCell(0);
+        if (celdaCodigo == null) return false;
+        String codigo = getStringContent(celdaCodigo);
+        return codigoValido(codigo.trim());
+    }
+
+    private static Boolean codigoValido(String codigo){
+        if(codigo.length() > 9) return false;
+        return (Character.isDigit(codigo.charAt(0)) ||
+           Character.isDigit(codigo.charAt(codigo.length()-1)) ||
+           codigo.charAt(codigo.length()-1) == 'W');
     }
 }
 
